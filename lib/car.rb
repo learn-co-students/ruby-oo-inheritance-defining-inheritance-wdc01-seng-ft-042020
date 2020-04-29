8097dbcc24fe38feaa416c0fc1916d3184a9664b
asharma414 <abhi.sharm.414@gmail.com>
2020-04-28 20:29:24 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,17 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+
+    def initialize(wheel_size, wheel_number)
+        super
+    end
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
+
+end
